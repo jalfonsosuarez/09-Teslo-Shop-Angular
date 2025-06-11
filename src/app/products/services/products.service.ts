a754c3a3b613629e383cc2597dd6454a21db1cc3
= <=>
2025-06-11 10:49:28 +0200

    Variables de entorno y parametrizacion (paginacion)

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -2,14 +2,31 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { ProductsResponse } from '@products/interfaces/product.interface';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = environment.baseUrl;
+
+interface Options {
+  limit?: number;
+  offset?: number;
+  gender?: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
   private http = inject(HttpClient);
 
-  getProducts(): Observable<ProductsResponse> {
+  getProducts(options: Options): Observable<ProductsResponse> {
+    const { limit = 9, offset = 0, gender = '' } = options;
+
     return this.http
-      .get<ProductsResponse>('http://localhost:3000/api/products')
+      .get<ProductsResponse>(`${baseUrl}/products`, {
+        params: {
+          limit,
+          offset,
+          gender,
+        },
+      })
       .pipe(
         tap((resp) => {
           console.log(resp);
