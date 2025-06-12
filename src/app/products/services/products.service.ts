db93df5621d3289b263bcbb01272cee2ed7d88aa
= <=>
2025-06-12 13:52:18 +0200

    Caché de producto.

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -20,6 +20,7 @@ export class ProductsService {
   private http = inject(HttpClient);
 
   private productsCache = new Map<string, ProductsResponse>();
+  private productCache = new Map<string, Product>();
 
   getProducts(options: Options): Observable<ProductsResponse> {
     const { limit = 9, offset = 0, gender = '' } = options;
@@ -41,6 +42,11 @@ export class ProductsService {
   }
 
   getProductByIsSlug(idSlug: string): Observable<Product> {
-    return this.http.get<Product>(`${baseUrl}/products/${idSlug}`);
+    if (this.productCache.has(idSlug)) {
+      return of(this.productCache.get(idSlug)!);
+    }
+    return this.http
+      .get<Product>(`${baseUrl}/products/${idSlug}`)
+      .pipe(tap((resp) => this.productCache.set(idSlug, resp)));
   }
 }
